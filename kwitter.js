d5db05dbda5d6006e851918abbd3c698bb49c41e
Deech-Teach <76897436+Deech-Teach@users.noreply.github.com>
2022-10-16 15:46:48 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,7 +1,10 @@
-function addUser(){
-user_name=document.getElementById("user_name").value;
 
-localStorage.setItem("use_name", user_name);
+function addUser() {
 
-window.location="kwitter_room.html";
+  user_name = document.getElementById("user_name").value;
+
+  localStorage.setItem("user_name", user_name);
+  
+    window.location = "kwitter_room.html";
 }
+
